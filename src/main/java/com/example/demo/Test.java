d40eca301b18bbaaf9354768f0705e9cc0e333a6
Jacky Liu <Changjiong.Liu@jetbrains.com>
2023-04-27 20:42:40 +0800

    test

---
+++ src/main/java/com/example/demo/Test.java
@@ -0,0 +1,18 @@
+package com.example.demo;
+
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("Hello");
+    }
+
+    /**
+     * 根据id获取相册信息
+     * @param id 相册id
+     * @return
+     * TODO Hello
+     */
+    DemoApplication hello(int id) {
+        return null;
+    }
+
+}
